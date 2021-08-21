611ee94221fe00da54d214fa72426f9d46100188
fraserdscott <fraserdscott@gmail.com>
2021-08-21 12:00:37 +0100

    Plz

--- pages/api/bundles/add.js
+++ pages/api/bundles/add.js
@@ -1,16 +1,9 @@
 import { connectToDatabase } from "../../../lib/mongodb";
 export default async (req, res) => {
     const { db } = await connectToDatabase();
+    const body = JSON.parse(req.body)
     const bundles = await db
         .collection("bundles")
-        .insertOne({
-            "_id": "hs",
-            "bundle": [
-                {
-                    "collectionAddress": "t",
-                    "tokenId": "1"
-                }
-            ]
-        })
+        .insertOne(body)
     res.json(bundles[0]);
 };

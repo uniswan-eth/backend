1d1154066bacdf2b121344c692c354f11c6e655a
fraserdscott <fraserdscott@gmail.com>
2021-08-21 11:52:10 +0100

    More testing

--- pages/api/bundles/add.js
+++ pages/api/bundles/add.js
@@ -3,13 +3,4 @@ export default async (req, res) => {
     const { db } = await connectToDatabase();
     await db
         .collection("bundles")
-        .insertOne({
-            "_id": "0x42856dfcc13c575e1fa5792bed13541b47e3e38ab8d2d7601925b9099f712e38",
-            "bundle": [
-                {
-                    "collectionAddress": "0x36a8377e2bb3ec7d6b0f1675e243e542eb6a4764",
-                    "tokenId": "1"
-                }
-            ]
-        })
 };

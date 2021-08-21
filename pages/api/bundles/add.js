f8563ba5b42a6d918284e998be08dbb0c78638cf
fraserdscott <fraserdscott@gmail.com>
2021-08-21 14:22:02 +0100

    Finally fix backend

--- pages/api/bundles/add.js
+++ pages/api/bundles/add.js
@@ -1,9 +1,29 @@
 import { connectToDatabase } from "../../../lib/mongodb";
-export default async (req, res) => {
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = async (req, res) => {
     const { db } = await connectToDatabase();
     const body = req.body;
     const bundles = await db
         .collection("bundles")
         .insertOne(body)
     res.json(bundles[0]);
-};
+}
+
+module.exports = allowCors(handler)

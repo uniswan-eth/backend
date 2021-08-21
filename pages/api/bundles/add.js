c784b5f5008f1af115397ebd6e4403a09faed5d6
fraserdscott <fraserdscott@gmail.com>
2021-08-21 11:50:40 +0100

    Testing

--- pages/api/bundles/add.js
+++ pages/api/bundles/add.js
@@ -1,10 +1,15 @@
 import { connectToDatabase } from "../../../lib/mongodb";
 export default async (req, res) => {
     const { db } = await connectToDatabase();
-    const { id } = req.query;
-    const bundles = await db
+    await db
         .collection("bundles")
-        .find({ _id: id })
-        .toArray();
-    res.json(bundles[0]);
+        .insertOne({
+            "_id": "0x42856dfcc13c575e1fa5792bed13541b47e3e38ab8d2d7601925b9099f712e38",
+            "bundle": [
+                {
+                    "collectionAddress": "0x36a8377e2bb3ec7d6b0f1675e243e542eb6a4764",
+                    "tokenId": "1"
+                }
+            ]
+        })
 };

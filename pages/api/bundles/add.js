b53a68154d6a6e11311729bed76e1b8f6a517adb
fraserdscott <fraserdscott@gmail.com>
2021-08-21 11:48:27 +0100

    Bug testing

--- pages/api/bundles/add.js
+++ pages/api/bundles/add.js
@@ -1,10 +1,10 @@
 import { connectToDatabase } from "../../../lib/mongodb";
 export default async (req, res) => {
     const { db } = await connectToDatabase();
-    const body = JSON.parse(req.body)
-    await db
+    const { id } = req.query;
+    const bundles = await db
         .collection("bundles")
-        .insertOne(body)
-        .then(result => console.log(`Successfully inserted item with _id: ${req.body}`))
-        .catch(err => console.error(`Failed to insert item: ${err}`))
+        .find({ _id: id })
+        .toArray();
+    res.json(bundles[0]);
 };

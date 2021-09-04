f52c0f25e11af0b690c9241584fbc4a7d12f8ba5
fraserdscott <fraserdscott@gmail.com>
2021-09-04 19:14:09 +0100

    Remove bundles endpoint

--- pages/api/bundles/index.js
+++
@@ -1,9 +0,0 @@
-import { connectToDatabase } from "../../../lib/mongodb";
-export default async (req, res) => {
-    const { db } = await connectToDatabase();
-    const bundles = await db
-        .collection("bundles")
-        .find({})
-        .toArray();
-    res.json(bundles);
-};

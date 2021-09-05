450df122b511ba07dadd7332632124dd8021a171
fraserdscott <fraserdscott@gmail.com>
2021-09-05 12:58:00 +0100

    Remove duplicate swaps that return same item

--- pages/api/options/[assetData].js
+++ pages/api/options/[assetData].js
@@ -1,15 +1,19 @@
 import { connectToDatabase } from "../../../lib/mongodb";
 
+var gotIts = [];
+
 function buildSwapChain(chain, orders) {
     var toReturn = []
     for (let i = 0; i < orders.length; i++) {
         if (
             orders[i].order.takerAssetData ===
             chain[chain.length - 1].order.makerAssetData
+            && !gotIts.includes(orders[i].order.makerAssetData)
         ) {
+            gotIts.push(orders[i].order.makerAssetData)
             const newChain = [...chain, orders[i]]
             toReturn.push(newChain);
-            if (newChain.length < 3) toReturn = toReturn.concat(buildSwapChain(newChain, orders));
+            if (newChain.length < 5) toReturn = toReturn.concat(buildSwapChain(newChain, orders));
         }
     }
     return toReturn;
@@ -24,10 +28,15 @@ export default async (req, res) => {
         .find({})
         .toArray();
 
+    // TODO: Check if the orders are active 
+    gotIts = [];
+
     var options = []
     for (let i = 0; i < orders.length; i++) {
         if (orders[i].order.takerAssetData === assetData) {
             options.push([orders[i]]);
+
+            gotIts.push(orders[i].order.makerAssetData)
 
             options = options.concat(buildSwapChain([orders[i]], orders));
         }

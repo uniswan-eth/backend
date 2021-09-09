ee197480650e6be84e2bfa4837a003189a0a6626
fraserdscott <fraserdscott@gmail.com>
2021-09-09 16:58:11 +0100

    Simplify query

--- pages/api/v3/orders/index.js
+++ pages/api/v3/orders/index.js
@@ -2,10 +2,9 @@ import { connectToDatabase } from "../../../../lib/mongodb";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
-    const { makerAddress } = req.query
     const orders = await db
         .collection("orders")
-        .find(makerAddress ? { makerAddress: makerAddress } : {})
+        .find(req.query)
         .toArray();
     res.json(orders);
 };

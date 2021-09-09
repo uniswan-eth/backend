f48cfd94172b11e610837b8ee1cffea749c5bca1
fraserdscott <fraserdscott@gmail.com>
2021-09-09 15:29:56 +0100

    Add filter

--- pages/api/v3/orders/index.js
+++ pages/api/v3/orders/index.js
@@ -2,9 +2,10 @@ import { connectToDatabase } from "../../../../lib/mongodb";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
+    const { makerAddress } = req.query
     const orders = await db
         .collection("orders")
-        .find({})
+        .find(makerAddress ? { makerAddress: makerAddress } : {})
         .toArray();
     res.json(orders);
 };

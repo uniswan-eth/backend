e5256da4252f098e4f20d84b8f34685ed06e2190
fraserdscott <fraserdscott@gmail.com>
2021-09-12 07:13:26 +0100

    FIlter cancelled or failed orders on backend

--- pages/api/v3/orders/index.js
+++ pages/api/v3/orders/index.js
@@ -1,4 +1,9 @@
+import EXCHANGEABI from "../../../../abis/Exchange.json";
 import { connectToDatabase } from "../../../../lib/mongodb";
+import { ethers } from "ethers";
+import { orderHashUtils } from "@0x/order-utils";
+
+const EXCHANGE_ADDRESS = "0x1f98206be961f98d0c2d2e5f7d965244b2f2129a";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
@@ -7,10 +12,23 @@ export default async (req, res) => {
         .find(req.query)
         .toArray();
     const apiOrders = []
+
+    var provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com');
+    var collection = new ethers.Contract(
+        EXCHANGE_ADDRESS,
+        EXCHANGEABI,
+        provider
+    );
+
     for (let i = 0; i < orders.length; i++) {
-        delete orders[i]._id;
-        delete orders[i].signature
-        apiOrders.push({ order: orders[i], metaData: {} })
+        var isCancelled = await collection.cancelled(orderHashUtils.getOrderHash(orders[i]));
+        var isFilled = await collection.filled(orderHashUtils.getOrderHash(orders[i]));
+
+        if (!isCancelled && isFilled.toNumber() === 0) {
+            delete orders[i]._id;
+            delete orders[i].signature;
+            apiOrders.push({ order: orders[i], metaData: {} });
+        }
     }
 
     res.json({

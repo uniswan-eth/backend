79b54afdfa9920d3179e27a001260e22172aee02
fraserdscott <fraserdscott@gmail.com>
2021-09-13 09:58:59 +0100

    Remove checks for speed

--- pages/api/v3/orders/index.js
+++ pages/api/v3/orders/index.js
@@ -1,8 +1,4 @@
-import EXCHANGEABI from "../../../../abis/Exchange.json";
 import { connectToDatabase } from "../../../../lib/mongodb";
-import { ethers } from "ethers";
-
-const EXCHANGE_ADDRESS = "0x1f98206be961f98d0c2d2e5f7d965244b2f2129a";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
@@ -16,25 +12,13 @@ export default async (req, res) => {
         .limit(perPage)
         .toArray();
 
-    const provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com');
-    const collection = new ethers.Contract(
-        EXCHANGE_ADDRESS,
-        EXCHANGEABI,
-        provider
-    );
+
 
     var validOrders = [];
     await Promise.all(
         orders.map(async (order) => {
-            const orderInfo = await collection.getOrderInfo(order);
+            validOrders.push({ order: order, metaData: {} });
 
-            if (orderInfo.orderStatus === 3) {
-                validOrders.push({ order: order, metaData: {} });
-            } else {
-                await db
-                    .collection("orders")
-                    .remove(order);
-            }
         })
     );
 
